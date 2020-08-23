6a4f46afe8b7fca3b48bfd025ba9ab7f71e108db
vividswan <vividswan@naver.com>
2020-08-24 02:58:55 +0900

    Create Member.java

---
+++ src/main/java/com/vividswan/blog/test/Member.java
@@ -0,0 +1,42 @@
+package com.vividswan.blog.test;
+
+public class Member {
+	private int id;
+	private String email;
+	private String username;
+	private String password;
+	
+	
+	public Member(int id, String email, String username, String password) {
+		this.id = id;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
