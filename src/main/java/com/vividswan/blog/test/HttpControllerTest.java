2a40c2752587d60e78c5d1e692e409272ff56d66
vividswan <vividswan@naver.com>
2020-08-24 02:58:50 +0900

    Create HttpControllerTest.java

---
+++ src/main/java/com/vividswan/blog/test/HttpControllerTest.java
@@ -0,0 +1,41 @@
+package com.vividswan.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// 사용자가 요청 -> 응답(HTML 파일) -> @Controller
+
+// 사용자가 요청 -> 응답(Data)
+@RestController
+public class HttpControllerTest {
+	
+	// 인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다.
+	// http://localhost:8080/http/get (select)
+	@GetMapping("/http/get")
+	public String getTest(Member m) { // id=1&username=vividswan&password=1234&email=vividswan@naver.com messageConverter (스프링부트)
+		return "get request : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+	}
+	
+	//http://localhost:8080/http/post (insert)
+	@PostMapping("/http/post") // text/plain, application/json
+	public String postTest(@RequestBody Member m) { // messageConverter (스프링부트)
+		return "post request : " +m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+	}
+	
+	//http://localhost:8080/http/put (update)
+	@PutMapping("/http/put")
+	public String putTest(@RequestBody Member m) {
+		return "put request : " +m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+	}
+	
+	//http://localhost:8080/http/delete (delete)
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete request";
+	}
+}
