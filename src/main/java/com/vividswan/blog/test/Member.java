4f4d1a9695150d4caadb7c1becc0ae069cda815a
vividswan <vividswan@naver.com>
2020-08-24 16:02:04 +0900

    Update Member.java

--- src/main/java/com/vividswan/blog/test/Member.java
+++ src/main/java/com/vividswan/blog/test/Member.java
@@ -1,42 +1,30 @@
 package com.vividswan.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @Getter
+// @Setter
+// @RequiredArgsConstructor
+
+@NoArgsConstructor
+@Data
 public class Member {
 	private int id;
 	private String email;
 	private String username;
 	private String password;
 	
-	
+	@Builder
 	public Member(int id, String email, String username, String password) {
 		this.id = id;
 		this.email = email;
 		this.username = username;
 		this.password = password;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 	
 	
 }
