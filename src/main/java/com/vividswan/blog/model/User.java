528325e535e663d2adbbed37a75fc5216cb6fe50
vividswan <vividswan@naver.com>
2020-08-25 01:11:52 +0900

    Update User.java

--- src/main/java/com/vividswan/blog/model/User.java
+++ src/main/java/com/vividswan/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +26,7 @@ import lombok.NoArgsConstructor;
 @Builder // 빌더패턴!!
 //ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity // User 클래스가 MySQL 테이블에 생성된다.
+// @DynamicInsert  // insert 시에 null인 필드를 제외시켜준다.
 public class User {
 	
 	@Id // Primary key
@@ -38,8 +42,10 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'") // 양 옆으로 작은 따옴표를 줘서 문자라는 것을 알려줘야함
-	private String role; // Enum을 쓰는게 좋다. // admin, user, manager -> 도메인(프로그래밍에서 도메인은 범위를 의미)
+	// @ColumnDefault("'user'") // 양 옆으로 작은 따옴표를 줘서 문자라는 것을 알려줘야함
+	
+	@Enumerated(EnumType.STRING) // 해당 enum이 String이라고 알려줌
+	private RoleType role; // Enum을 쓰는게 좋다. // admin, user, manager -> 도메인(프로그래밍에서 도메인은 범위를 의미)
 	
 	@CreationTimestamp // 값을 비워두고 insert해도 시간이 자동 입력
 	private Timestamp createDate;
