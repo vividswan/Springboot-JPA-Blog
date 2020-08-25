13f2fe6bd45507c0c24f9a22e07bf38d429c0bb5
vividswan <vividswan@naver.com>
2020-08-26 00:39:53 +0900

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/vividswan/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.vividswan.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(value=Exception.class)
+	public String handleException(Exception e) {
+		return "<h1>"+e.getMessage()+"</h1>";
+	}
+}
