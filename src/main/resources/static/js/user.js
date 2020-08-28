5c3336543f507ba221dfc140ab7f19eb11cfd2f9
vividswan <vividswan@naver.com>
2020-08-28 17:43:05 +0900

    Modify Project address

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -18,13 +18,13 @@ let index = {
     // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청
     $.ajax({
       type: "POST",
-      url: "/blog/api/user",
+      url: "/api/user",
       data: JSON.stringify(data), // http body 데이터
       contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지(MIME)
       //dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json 이라면) => javascript object로 변경
     }).done(function (response) {
       alert('회원가입이 완료되었습니다.');
-      location.href = "/blog";
+      location.href = "/";
     }).fail(function (error) {
       alert(JSON.stringify(error));
     });
@@ -37,12 +37,12 @@ let index = {
 
     $.ajax({
       type: "POST",
-      url: "/blog/api/login",
+      url: "/api/login",
       data: JSON.stringify(data),
       contentType: "application/json; charset=utf-8",
     }).done(function (response) {
       alert('로그인이 완료되었습니다.');
-      location.href = "/blog";
+      location.href = "/";
     }).fail(function (error) {
       alert(JSON.stringify(error));
     });
