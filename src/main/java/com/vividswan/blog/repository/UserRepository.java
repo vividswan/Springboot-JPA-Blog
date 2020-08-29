35cda2e3b0c33b1d3ec4e895494811572d6f676b
vividswan <vividswan@naver.com>
2020-08-29 22:48:00 +0900

    Update UserRepository.java

--- src/main/java/com/vividswan/blog/repository/UserRepository.java
+++ src/main/java/com/vividswan/blog/repository/UserRepository.java
@@ -8,10 +8,11 @@ import com.vividswan.blog.model.User;
 // 자동으로 bean에 등록됨(스프링 IoC)
 // @Repository 생략가능!!
 public interface UserRepository extends JpaRepository<User, Integer>{
-	// JPA Naming 쿼리
-	// SELECT * FROM user WHERE username = ?1 AND password = ?2;
-	User findByUsernameAndPassword(String username, String password);
+}
+
+// JPA Naming 쿼리
+// SELECT * FROM user WHERE username = ?1 AND password = ?2;
+//User findByUsernameAndPassword(String username, String password);
 	
 //	@Query(value="SELECT * FROM user WHERE username = ?1 AND password = ?2;", nativeQuery = true )
 //	User login(String username, String password);
-}
