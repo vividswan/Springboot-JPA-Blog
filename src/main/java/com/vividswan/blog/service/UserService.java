9ef3b5abb53915f57a20bfaf5da511056dba6df9
vividswan <vividswan@naver.com>
2020-08-29 22:48:05 +0900

    Update UserService.java

--- src/main/java/com/vividswan/blog/service/UserService.java
+++ src/main/java/com/vividswan/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.vividswan.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vividswan.blog.model.RoleType;
 import com.vividswan.blog.model.User;
 import com.vividswan.blog.repository.UserRepository;
 
@@ -11,15 +13,22 @@ import com.vividswan.blog.repository.UserRepository;
 public class UserService {
 	
 	@Autowired
-	UserRepository userRepository;
+	private BCryptPasswordEncoder encoder;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Transactional
 	public void saveUser(User user) {
+		user.setRole(RoleType.USER);
+		String rawPassword = user.getPassword();
+		String encPassword= encoder.encode(rawPassword);
+		user.setPassword(encPassword);
 		userRepository.save(user);
 	}
 	
-	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
-	public User login(User user) {
-		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-	}
+//	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
+//	public User login(User user) {
+//		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+//	}
 }
