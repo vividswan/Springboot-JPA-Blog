7a135ad0a1c2be1e41f7fc61830582c9a07832a7
vividswan <vividswan@naver.com>
2020-08-31 23:34:33 +0900

    Update Board.java

--- src/main/java/com/vividswan/blog/model/Board.java
+++ src/main/java/com/vividswan/blog/model/Board.java
@@ -40,7 +40,6 @@ public class Board {
 	@Lob // 대용량 데이터
 	private String content; // 섬머노트 라이브러리 (<html> 태그가 섞여서 디자인 됨.)
 	
-	@ColumnDefault("0")
 	private int count; // 조회수
 	
 	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) // mappedBy-> 연관관계의 주인이 아니므로, DB에 FK키를 만들지 말라는 의미, EAGER 전략 -> 무조건 가져오기
