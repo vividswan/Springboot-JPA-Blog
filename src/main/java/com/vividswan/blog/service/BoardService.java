0c7d490a09a8a3713e5110a5bd8a2c5af77049b1
vividswan <vividswan@naver.com>
2020-08-31 23:34:23 +0900

    Update BoardService.java

--- src/main/java/com/vividswan/blog/service/BoardService.java
+++ src/main/java/com/vividswan/blog/service/BoardService.java
@@ -1,8 +1,12 @@
 package com.vividswan.blog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Pageable;
 
 import com.vividswan.blog.config.auth.PrincipalDetail;
 import com.vividswan.blog.model.Board;
@@ -20,6 +24,10 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	public Page<Board> getBoards(Pageable pageable){
+		return boardRepository.findAll(pageable);
 	}
 	
 //	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
