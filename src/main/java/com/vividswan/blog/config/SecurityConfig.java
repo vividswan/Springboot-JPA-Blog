4d836b75e69bb8f358313017aa0655d4797b7b04
vividswan <vividswan@naver.com>
2020-08-31 13:56:32 +0900

    Update SecurityConfig.java

--- src/main/java/com/vividswan/blog/config/SecurityConfig.java
+++ src/main/java/com/vividswan/blog/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.vividswan.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.vividswan.blog.config.auth.PrincipalDetailService;
 
 
 @Configuration // 빈등록 (IoC 관리)
@@ -14,10 +18,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근 하면 권한 및 인증을 미리 체크
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private PrincipalDetailService principalDetailServicel;
+	
 	@Bean // IoC가 관리
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	// 시큐리티가 대신 로그인 할 때 password를 가로채기 한다.
+	// 해당 password가 뭘로 해쉬과 되어있는지 알아야함
+	// 그래야 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교가능
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailServicel).passwordEncoder(encodePWD());
+	}
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,7 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.authenticated()
 		.and()
 			.formLogin()
-			.loginPage("/auth/loginForm");
+			.loginPage("/auth/loginForm")
+			.loginProcessingUrl("/auth/loginProc")
+			.defaultSuccessUrl("/"); // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준다.
 	}
 
 }
