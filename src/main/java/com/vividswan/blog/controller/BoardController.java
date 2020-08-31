544f2c30415c88200864067bb24b8a6423a5fbd8
vividswan <vividswan@naver.com>
2020-08-31 21:56:04 +0900

    Update BoardController.java

--- src/main/java/com/vividswan/blog/controller/BoardController.java
+++ src/main/java/com/vividswan/blog/controller/BoardController.java
@@ -1,7 +1,10 @@
 package com.vividswan.blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.vividswan.blog.config.auth.PrincipalDetail;
 
 @Controller
 public class BoardController {
@@ -11,4 +14,10 @@ public class BoardController {
 		// /WEB-INF/views/index.jsp
 		return "index";
 	}
+	
+	@GetMapping("/board/saveForm")
+	public String saveForm() {
+		return "/board/saveForm";
+	}
+	
 }
