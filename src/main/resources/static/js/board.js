9466473c28256f823005bc880dfda2484f1221e9
vividswan <vividswan@naver.com>
2020-09-01 01:53:20 +0900

    Update board.js

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index = {
     $("#btn-save").on("click", () => {
       this.save();
     });
+    $("#btn-delete").on("click", () => {
+      this.deleteById();
+    });
   },
   save: function () {
     let data = {
@@ -13,11 +16,23 @@ let index = {
     $.ajax({
       type: "POST",
       url: "/api/saveProc",
-      data: JSON.stringify(data), 
+      data: JSON.stringify(data),
       contentType: "application/json; charset=utf-8",
       //dataType: "json"
     }).done(function (response) {
       alert('글 작성이 완료되었습니다.');
+      location.href = "/";
+    }).fail(function (error) {
+      alert(JSON.stringify(error));
+    });
+  },
+  deleteById: function () {
+    let id = $("#id").text();
+    $.ajax({
+      type: "DELETE",
+      url: "/api/delete/" + id,
+    }).done(function (response) {
+      alert('글 삭제가 완료되었습니다.');
       location.href = "/";
     }).fail(function (error) {
       alert(JSON.stringify(error));
