6eda5b5bc381dbb8058307d63851c756ac16d818
vividswan <vividswan@naver.com>
2020-09-01 01:53:26 +0900

    Update BoardService.java

--- src/main/java/com/vividswan/blog/service/BoardService.java
+++ src/main/java/com/vividswan/blog/service/BoardService.java
@@ -1,14 +1,11 @@
 package com.vividswan.blog.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.domain.Pageable;
 
-import com.vividswan.blog.config.auth.PrincipalDetail;
 import com.vividswan.blog.model.Board;
 import com.vividswan.blog.model.User;
 import com.vividswan.blog.repository.BoardRepository;
@@ -26,8 +23,22 @@ public class BoardService {
 		boardRepository.save(board);
 	}
 	
+	@Transactional(readOnly = true)
 	public Page<Board> getBoards(Pageable pageable){
 		return boardRepository.findAll(pageable);
+	}
+	
+	@Transactional(readOnly = true)
+	public Board getBoard(int id) {
+		return boardRepository.findById(id)
+					.orElseThrow(()->{
+						return new IllegalArgumentException("해당 하는 번호의 게시물이 없습니다.");
+					});
+	}
+	
+	@Transactional
+	public void deleteBoard(int id) {
+		boardRepository.deleteById(id);
 	}
 	
 //	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
