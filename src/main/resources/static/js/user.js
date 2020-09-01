44de34099c9def395803f1002744b8ecbd2b40f3
vividswan <vividswan@naver.com>
2020-09-02 00:01:51 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
     $("#btn-save").on("click", () => { // function(){}을 ()=>{} this를 바인딩하기 위해서!! function()에서 this -> window의 this가 된다, arrow function에서의 this -> index!
       this.save();
     });
+    $("#btn-update").on("click", () => {
+      this.update();
+    });
   },
   save: function () {
     let data = {
@@ -21,6 +24,25 @@ let index = {
       //dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json 이라면) => javascript object로 변경
     }).done(function (response) {
       alert('회원가입이 완료되었습니다.');
+      location.href = "/";
+    }).fail(function (error) {
+      alert(JSON.stringify(error));
+    });
+  },
+   update: function () {
+    let data = {
+      id:$("#id").val(),
+      password: $("#password").val(),
+      email: $("#email").val(),
+    };
+
+    $.ajax({
+      type: "PUT",
+      url: "/uers/update",
+      data: JSON.stringify(data),
+      contentType: "application/json; charset=utf-8",
+    }).done(function (response) {
+      alert('회원수정이 완료되었습니다.');
       location.href = "/";
     }).fail(function (error) {
       alert(JSON.stringify(error));
