97b7305dfb6e82f1195147e6b7d2a2655c2971d8
vividswan <vividswan@naver.com>
2020-09-02 00:01:55 +0900

    Update UserService.java

--- src/main/java/com/vividswan/blog/service/UserService.java
+++ src/main/java/com/vividswan/blog/service/UserService.java
@@ -1,6 +1,7 @@
 package com.vividswan.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +26,23 @@ public class UserService {
 		String encPassword= encoder.encode(rawPassword);
 		user.setPassword(encPassword);
 		userRepository.save(user);
+	}
+	
+	@Transactional
+	public void update(User requestUser) {
+		// 수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화 된 User 오브젝트를 수정
+		// select를 해서 User 오브젝트를 DB로부터 가져오는 이유는 영속하를 하기 위함
+		// 영속화된 오브젝트를 변경하면 자동으로 DB에 update문을 날려줌
+		User user = userRepository.findById(requestUser.getId())
+				.orElseThrow(()-> {
+					return new IllegalArgumentException("사용자를 찾을 수 없습니다.");
+				});
+		String rawPassword = requestUser.getPassword();
+		String encPassword = encoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		user.setEmail(requestUser.getEmail());
+		// 회원수정 함수 종료 시 -> 서비스 종료 -> 트랜잭션 종료 -> commit이 자동으로 됨
+		// 영속화된 persistance 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌.
 	}
 	
 //	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
