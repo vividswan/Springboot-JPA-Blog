ac71b7af2cdfe1087c49c5b365e013c74a132b7f
vividswan <vividswan@naver.com>
2020-09-02 23:59:55 +0900

    Update UserApiController.java

--- src/main/java/com/vividswan/blog/api/UserApiController.java
+++ src/main/java/com/vividswan/blog/api/UserApiController.java
@@ -2,6 +2,10 @@ package com.vividswan.blog.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,11 +21,21 @@ import com.vividswan.blog.service.UserService;
 public class UserApiController {
 	
 	@Autowired
-	UserService userService;
+	private UserService userService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	@PutMapping("/uers/update")
 	public ResponseDto<Integer> update(@RequestBody User user){
 		userService.update(user);
+		// 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음.
+		// 하지만 세션값은 변경되지 않은 상태이기 때문에 직접 세션값을 변경해줘야 함
+		// 세션등록
+		
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 	
