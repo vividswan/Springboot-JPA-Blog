cd2b1f7bc07823c446ff7dcdabfabaff8a09fb11
vividswan <vividswan@naver.com>
2020-09-03 00:00:01 +0900

    Update SecurityConfig.java

--- src/main/java/com/vividswan/blog/config/SecurityConfig.java
+++ src/main/java/com/vividswan/blog/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.vividswan.blog.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +19,14 @@ import com.vividswan.blog.config.auth.PrincipalDetailService;
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근 하면 권한 및 인증을 미리 체크
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+
 	@Autowired
 	private PrincipalDetailService principalDetailServicel;
 	
