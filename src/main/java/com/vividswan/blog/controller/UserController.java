d83dd84a93e27a6b10cc869b8e471f16db795ae4
vividswan <vividswan@naver.com>
2020-09-04 15:33:54 +0900

    Update UserController.java

--- src/main/java/com/vividswan/blog/controller/UserController.java
+++ src/main/java/com/vividswan/blog/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.vividswan.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -19,5 +27,35 @@ public class UserController {
 	@GetMapping("/user/updateForm")
 	public String userUpdate() {
 		return "user/updateForm";
+	}
+	
+	@GetMapping("/auth/kakao/callback")
+	public@ResponseBody String kakaoCallback(String code) { // Data를 리턴해주는 컨트롤러 함수
+		// POST 방식으로 key=value 데이터를 요청 (카카오쪽으로)
+		
+		RestTemplate rt = new RestTemplate();
+		// HttpHeader 오브젝트 생성
+		HttpHeaders  headers = new HttpHeaders();
+		headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+		
+		// HttpBody 오브젝트 생성
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "c11e5c83f15754e7aee244b5b9b821ea");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		// HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+		HttpEntity<MultiValueMap<String, String>>	kakaoTokenRequest = new HttpEntity<>(params, headers);
+		
+		// Http 요청하기 - Post 방식으로 - 그리고 Response 변수의 응답 받음.
+		ResponseEntity<String> response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod	.POST,
+				kakaoTokenRequest,
+				String.class
+		);
+		
+		return "카카오 토큰 요청 완료 : 토큰 요청에 대한 응답 : " +response;
 	}
 }
