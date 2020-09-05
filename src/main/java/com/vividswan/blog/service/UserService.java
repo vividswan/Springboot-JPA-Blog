4ae1bbf61ab2c7586a5bc8dcd1bcf1be678f9b02
vividswan <vividswan@naver.com>
2020-09-06 02:43:04 +0900

    Update UserService.java

--- src/main/java/com/vividswan/blog/service/UserService.java
+++ src/main/java/com/vividswan/blog/service/UserService.java
@@ -37,12 +37,23 @@ public class UserService {
 				.orElseThrow(()-> {
 					return new IllegalArgumentException("사용자를 찾을 수 없습니다.");
 				});
-		String rawPassword = requestUser.getPassword();
-		String encPassword = encoder.encode(rawPassword);
-		user.setPassword(encPassword);
-		user.setEmail(requestUser.getEmail());
+		
+		// Check Validate
+		if(user.getOauth()==null || user.getOauth().equals("")) {
+			String rawPassword = requestUser.getPassword();
+			String encPassword = encoder.encode(rawPassword);
+			user.setPassword(encPassword);
+			user.setEmail(requestUser.getEmail());
+		}
 		// 회원수정 함수 종료 시 -> 서비스 종료 -> 트랜잭션 종료 -> commit이 자동으로 됨
 		// 영속화된 persistance 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌.
+	}
+
+	public User findUser(String username) {
+		User user = userRepository.findByUsername(username).orElseGet(()->{
+			return new User();
+		});
+		return user;
 	}
 	
 //	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
