586e2bf405d8ad51092c4d2edefdd54044c074c4
vividswan <vividswan@naver.com>
2020-09-06 22:14:03 +0900

    Update BoardService.java

--- src/main/java/com/vividswan/blog/service/BoardService.java
+++ src/main/java/com/vividswan/blog/service/BoardService.java
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vividswan.blog.model.Board;
+import com.vividswan.blog.model.Reply;
 import com.vividswan.blog.model.User;
 import com.vividswan.blog.repository.BoardRepository;
+import com.vividswan.blog.repository.ReplyRepository;
 
 @Service
 public class BoardService {
 	
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	@Autowired
+	private ReplyRepository replyRepository;
 	
 	@Transactional
 	public void saveBoard(Board board, User user) {
@@ -49,6 +54,16 @@ public class BoardService {
 				});
 		board.setTitle(requestBoard.getTitle());
 		board.setContent(requestBoard.getContent());
+	}
+
+	@Transactional
+	public void saveReply(User user, Reply reply, int boardId) {
+		Board board = boardRepository.findById(boardId).orElseThrow(()->{
+			return new IllegalArgumentException("댓글 작성 실패 : 일치하는 게시판이 없습니다.");
+		});
+		reply.setBoard(board);
+		reply.setUser(user);
+		replyRepository.save(reply);
 	}
 	
 //	@Transactional(readOnly = true) // Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료 (정합성)
