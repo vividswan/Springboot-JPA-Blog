45bc32e3114690fbdb01052683040a96ee234938
vividswan <vividswan@naver.com>
2020-09-07 03:00:54 +0900

    Update ReplyRepository.java

--- src/main/java/com/vividswan/blog/repository/ReplyRepository.java
+++ src/main/java/com/vividswan/blog/repository/ReplyRepository.java
@@ -1,8 +1,14 @@
 package com.vividswan.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.vividswan.blog.model.Reply;
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer>{
-
+	
+	@Modifying
+	@Query(value = "INSERT INTO reply(userId, boardId, content, createDate) VALUES(?1, ?2, ?3, now())",nativeQuery = true)
+	int createReply(int userId, int boardId, String content);
 }
