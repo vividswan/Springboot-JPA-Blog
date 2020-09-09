3120285d9bdf327155e9c2419ec8711acb3ca3c3
vividswan <vividswan@naver.com>
2020-09-09 09:59:49 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -23,7 +23,12 @@ let index = {
       contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지(MIME)
       //dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json 이라면) => javascript object로 변경
     }).done(function (response) {
-      alert('회원가입이 완료되었습니다.');
+      if(response.status === 500){
+        alert('회원가입에 실패하였습니다.');
+      }
+      else{
+        alert('회원가입이 완료되었습니다.');
+      }
       location.href = "/";
     }).fail(function (error) {
       alert(JSON.stringify(error));
